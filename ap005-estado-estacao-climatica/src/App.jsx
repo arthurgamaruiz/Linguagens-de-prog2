eadcd55fd1501012c5b8083630dee01eeef9a186
Arthur Ruiz <23.01445-8@maua.br>
2026-04-25 12:31:35 -0300

    learn(ap05-efeito): implementa efeito e loading na aplicacao da estacao climatica

--- ap005-estado-estacao-climatica/src/App.jsx
+++ ap005-estado-estacao-climatica/src/App.jsx
@@ -1,7 +1,10 @@
-//rafce
-import React, { useEffect, useState } from "react"
-
+import React, {useEffect, useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import EstacaoClimatica from './EstacaoClimatica'
+import Loading from './Loading'
 const App = () => {
+  
   const [latitude, setLatitude] = useState(null)
   const [longitude, setLongitude] = useState(null)
   const [estacao, setEstacao] = useState(null)
@@ -9,93 +12,85 @@ const App = () => {
   const [icone, setIcone] = useState(null)
   const [mensagemDeErro, setMensagemDeErro] = useState(null)
 
-  const obterEsatacao = (dataAtual, latidude) => {
-    const d1 = new Date(dataAtual.getFullYear(), 5, 21)   //21/06/anoAtual
+  const obterEstacao = (dataAtual, latitude) => {
+    const d1 = new Date(dataAtual.getFullYear(), 5, 21)
     const d2 = new Date(dataAtual.getFullYear(), 8, 24)
     const d3 = new Date(dataAtual.getFullYear(), 11, 22)
     const d4 = new Date(dataAtual.getFullYear(), 2, 21)
 
-    const estaNoSul = latidude<0;
-    if(dataAtual >= d1 && dataAtual < d2)
-      return estaNoSul ? 'Inverno':'Verão'
-    if(dataAtual >= d2 && dataAtual < d3)
-      return estaNoSul ? 'Primavera':'Outono'
-    if(dataAtual >= d3 || dataAtual < d4)
+    const estaNoSul = latitude < 0
+    if (dataAtual >= d1 && dataAtual < d2)
+      return estaNoSul ? 'Inverno' : 'Verão'
+
+    if (dataAtual >= d2 && dataAtual < d3)
+      return estaNoSul ? 'Primavera' : 'Outono'
+
+    if (dataAtual >= d3 || dataAtual < d4)
       return estaNoSul ? 'Verão' : 'Inverno'
     return estaNoSul ? 'Outono' : 'Primavera'
 
   }
-  //ícones das estações 
-  const icones = {
-    'Primavera':  'leaf' ,
-    'Verão':  'umbrella-beach',
-    'Outono': 'leaf',
-    'Inverno':'snowman'
 
+  const icones = {
+    'Primavera': 'fa-leaf fa-2x',
+    'Verão': 'fa-sun fa-2x',
+    'Outono': 'fa-solid fa-clover fa-2x',
+    'Inverno': 'fa-snowflake fa-2x'
   }
+
   const obterLocalizacao = () => {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         const dataAtual = new Date()
-        //chamar a função obterEstacao para obter a estação climática do usuário
-        const estacao = obterEsatacao(dataAtual, position.coords.latitude)
-        //obter ícone apropriado 
-        const icone = icones[estacao]
+        //chamar a função obter estação para obter a estação climática do usuário
+        //obter ícone apropriado
         //atualizar estado
+        //ajustar a jsx para que por enquanto, somente o nome da estação apareça
+        
+        const latitude = position.coords.latitude
+        const longitude = position.coords.longitude
+        const estacao = obterEstacao(dataAtual, latitude)
+        const data = dataAtual
+        const icone = icones[estacao]
+        
+        setLatitude(latitude)
+        setLongitude(longitude)
+        setData(data)
         setEstacao(estacao)
         setIcone(icone)
-        setLatitude(position.coords.latitude)
-        setLongitude(position.coords.longitude)
-        setData(dataAtual.toLocaleTimeString())
-        //ajustar a jsx para que, por enquanto, somente o nome da estação apareça
-      },
-      (erro) => {
-        setMensagemDeErro(erro)
-        console.log(`Erro: ${erro.toString()}`);
+      }, 
+      (error) => {
+        setMensagemDeErro('É preciso liberar a localização para ver a estação.')
+        console.log(`Erro: ${error.toString()}`)
       })
   }
-  //obtém a localização apenas uma vez (logo após a primeira renderização)
+
   useEffect(() => {
     obterLocalizacao()
   }, [])
-  
+
   return (
-    <div className="container mt-2">        
+    <div className="container mt-3">
       <div className="row justify-content-center">
         <div className="col-12 col-md-8">
-          <div className="card">
-            <div className="card-body">
-              <div 
-                style = {{height: '6rem'}}
-                className="d-flex align-items-center border rounded mb-2">
-                  <i className={`fa-solid fa-5x fa-${icone}`}></i>
-                  <p className="w-75 ms-3 text-center fs-1">
-                    {estacao}
-                  </p>
-              </div>
-              <div>
-                <p className="text-center">
-                  {
-                    latitude ?
-                      `Coordenadas: ${latitude}, ${longitude}. Data: ${data}` 
-                    :
-                    mensagemDeErro ? 
-                    mensagemDeErro
-                    :
-                    'Clique no botão para saber sua estação'
-                  }
-                </p>
-              </div>
-              <button
-                onClick={obterLocalizacao}
-                className='btn btn-outline-primary w-100 mt-2'>
-                Qual a minha estação ?
-              </button>
-            </div>
-          </div>
+          {
+            (!latitude && !mensagemDeErro)
+            ? <Loading mensagem='Por favor, responda à solicitação de localização.'/>
+            : mensagemDeErro
+              ? <p className='border rounded p-2 fs-4 text-center'>
+                  É preciso dar permissão para acesso á localização. 
+                  Atualize a página novamente.
+              </p>
+              : <EstacaoClimatica
+                  icone = {icone}
+                  estacao={estacao}
+                  latitude={latitude}
+                  longitude={longitude}
+                  obterLocalizacao={obterLocalizacao}/>
+          }
         </div>
       </div>
-    </div>    
+    </div>
   )
 }
 
